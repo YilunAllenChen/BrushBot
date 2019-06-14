de0dbc3ac7ebd3d0893cfd3d34dd687e5f9ec791
YilunAllenChen <allenchen@gatech.edu>
2019-06-14 11:21:23 -0400

    Adding schematics; Fixed main.py

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-from machine import Pin
-led = Pin(5,Pin.OUT)
-led.value(0)
+import machine
+import sys
+import utime
+
+# Pin definitions
+repl_button = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_UP)
+
+# Wait for button 0 to be pressed, and then exit
+while True:
+
+    # If button 0 is pressed, drop to REPL
+    if repl_button.value() == 0:
+        print("Dropping to REPL")
+        sys.exit()
+
+    # Do nothing
+    utime.sleep_ms(1)
