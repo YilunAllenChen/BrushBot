6278dba89234ea788bddd8dcd750bc3088954e7f
YilunAllenChen <allenchen@gatech.edu>
2019-07-24 16:36:38 -0400

    Better looking initialization messages

--- Firmware/main.py
+++ Firmware/main.py
@@ -6,46 +6,54 @@ import vl6180_Driver
 from ina219 import INA219
 from configureVL6180 import configureVL6180
 
+
+############    Initiazation Settings - Don't Change    ######################
+print("\nInitializing devices...\n")
+# Default Reset Pin Definition. Don't Change.
+repl_button = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_UP)
+
 #Sensor-enable Initialization
 sensor_en = machine.Pin(18, machine.Pin.OUT)
 sensor_en.value(1)
+
 #Motor-enable Initialization
 sensor_en = machine.Pin(19, machine.Pin.OUT)
 sensor_en.value(1)
 
-#LED neoPixel on Pin 37 setup
-try:
-    neoPixel = neopixel.NeoPixel(machine.Pin(23),2)
-except:
-    print("Unable to initialize neoPixel.")
-
-
 #I2C Definition. Don't change.
 i2c = machine.I2C(-1,machine.Pin(22),machine.Pin(21))
 
-# Default Reset Pin Definition. Don't Change.
-repl_button = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_UP)
-
 # Default LED pin. Bot Running Indicator. This LED is right to the left of the ESP board.
 led = machine.Pin(32, machine.Pin.OUT)
+
+#LED neoPixel on Pin 37 setup
+try:
+    neoPixel = neopixel.NeoPixel(machine.Pin(23),2)
+    print("\033[92m{}\033[00m" .format("Successfully") + " configured neoPixels")
+except:
+    print("\033[91m{}\033[00m".format("Failed") + " to configure neoPixels")
 
 # Laser Sensor Initialization
 sensors = []
 for addr in range (41,48):  # VL6180 sensors should be on i2c addresses 41-48.
     try:
         sensors.append(vl6180_Driver.Sensor(i2c,addr))
-        print("VL6180x sensor at address " + str(addr) + " is configured successfully." )
+        print("\033[92m{}\033[00m" .format("Successfully") + " configured VL6180x sensor at address " + str(addr))
     except:
-        print("Unable to initialize VL6180x sensor on i2c address " + str(addr))
-
+        print("\033[91m{}\033[00m".format("Failed") + " to initialize VL6180x sensor on i2c address " + str(addr))
 # Current Sensor Initialization
 shunt_resistance = 0.05      # Change if needed
 try:
     ina = INA219(shunt_resistance, i2c)
     ina.configure()
-    print("INA219 sensor at address " + str(ina._address) + " is configured successfully.")
+    print("\033[92m{}\033[00m".format("Successfully") + " configured INA219 sensor at address " + str(ina._address))
 except:
-    print("Unable to initialize INA219 sensor on address 65.")
+    print("\033[91m{}\033[00m".format("Failed") + " to initialize INA219 sensor on address 65.")
+####################    End of Initialization   #################################
+
+
+
+
 
 # Blink forever
 while True:
@@ -70,7 +78,7 @@ while True:
         print("Power: %.3f mW" % ina.power())
         print("Current Overflow? " + str(ina.current_overflow()))
     except:
-        print("INA219 sensor is malfunctioning.")
+        print("\033[92m {}\033[00m".format("INA219 sensor is malfunctioning."))
     
     led.value(1)
     neoPixel[1] = (0,0,10)
